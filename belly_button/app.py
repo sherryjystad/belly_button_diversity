72b2429a613e98c3cab18da34992bdc054a2d69c
sherryjystad <38591924+sherryjystad@users.noreply.github.com>
2018-11-16 13:41:31 -0800

    Add files via upload

--- belly_button/app.py
+++ belly_button/app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,inspect
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -18,40 +18,40 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
+# Establishing connection to database
+#engine = create_engine("sqlite:///db/bellybutton.sqlite")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
 Base.prepare(db.engine, reflect=True)
+Base.classes.keys()
 
 # Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
 Samples = Base.classes.samples
+Samples_Metadata = Base.classes.sample_metadata
 
-
+# Flask Homepage
 @app.route("/")
 def index():
-    """Return the homepage."""
+    # Return the homepage
     return render_template("index.html")
-
 
 @app.route("/names")
 def names():
-    """Return a list of sample names."""
+    # Return a list of sample names
 
     # Use Pandas to perform the sql query
     stmt = db.session.query(Samples).statement
     df = pd.read_sql_query(stmt, db.session.bind)
-
+    
     # Return a list of the column names (sample names)
     return jsonify(list(df.columns)[2:])
 
-
 @app.route("/metadata/<sample>")
 def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
+    # Return the MetaData for a given sample
     sel = [
         Samples_Metadata.sample,
         Samples_Metadata.ETHNICITY,
@@ -60,8 +60,8 @@ def sample_metadata(sample):
         Samples_Metadata.LOCATION,
         Samples_Metadata.BBTYPE,
         Samples_Metadata.WFREQ,
-    ]
-
+    ] 
+   
     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
 
     # Create a dictionary entry for each row of metadata information
@@ -74,27 +74,56 @@ def sample_metadata(sample):
         sample_metadata["LOCATION"] = result[4]
         sample_metadata["BBTYPE"] = result[5]
         sample_metadata["WFREQ"] = result[6]
-
-    print(sample_metadata)
     return jsonify(sample_metadata)
-
 
 @app.route("/samples/<sample>")
 def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+    # Return otu_ids, otu_labels,and sample_values
     stmt = db.session.query(Samples).statement
     df = pd.read_sql_query(stmt, db.session.bind)
 
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
+    # Filter the data based on the sample number and  # only keep rows with values above 1
     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
+    
+    # Sort by descending
+    sample_data = sample_data.sort_values(by=sample, ascending=0)
+
+    # Prepare Data for graphing in front end.
     data = {
         "otu_ids": sample_data.otu_id.values.tolist(),
         "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
+        "otu_labels": sample_data.otu_label.tolist()
+    }    
     return jsonify(data)
+
+@app.route("/samples10/<sample>")
+def samples10(sample):
+
+    # Return otu_ids, otu_labels,and sample_values
+    stmt = db.session.query(Samples).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Filter the data based on the sample number and  # only keep rows with values above 1
+    sample_data = df.loc[df[sample] > 1, ["otu_id","otu_label", sample]]
+
+    sample_ten = sample_data.loc[:, ['otu_id', sample]].sort_values(sample, ascending=False)
+    sample_ten = sample_ten.astype(int)
+
+    # Slice for top ten and json for 
+    otu_ids_sorted = list(sample_ten.iloc[:10, 0])
+    otu_labels_sorted = list(sample_ten.iloc[:10, 1])
+
+    #samples_sorted = list(sample_ten.iloc[:10, 2])
+    #print("sample sorted", samples_sorted)    
+
+    # Prepare Data for graphing in front end.
+    data10 = {
+            
+        "otu_ids": otu_ids_sorted,#.index.values.tolist(),
+        "sample_values": otu_labels_sorted,#.values.tolist(),
+       # "otu_labels": samples_sorted#.values.tolist()
+    }    
+    return jsonify(data10)
 
 
 if __name__ == "__main__":
