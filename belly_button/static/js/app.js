6a01c606aabe59b6a4b2cb10da44ba6c4b739f90
sherryjystad <38591924+sherryjystad@users.noreply.github.com>
2018-11-17 15:23:31 -0800

    Add files via upload

--- belly_button/static/js/app.js
+++ belly_button/static/js/app.js
@@ -1,47 +1,80 @@
-// Getting references
-// var selDataset = document.getElementById("selDataset");
-// var PIE = document.getElementById("pie");
-// var BUBBLE = document.getElementById("bubble");
-// var Gauge = document.getElementById("gauge");
-
-function buildMetadata(sample) {
-
-  // @TODO: Complete the following function that builds the metadata panel
-  var panel = document.getElementById("sample-metadata");
-  tbody.html("");
-
-	filteredData.forEach((sample) => {
-		var row = tbody.append("tr");
-		Object.entries(sample).forEach(([key, value]) => {
-			console.log(key, value);
-			var cell = row.append("td");
-			cell.text(value);
-		});
-	});
-};
-
-  // Use `d3.json` to fetch the metadata for a sample
+function buildMetadata(sample_data) {
+  d3.json(`/metadata/${sample_data}`).then((data) => {
     // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+    PANEL.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(data).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key}: ${value}`);
+    });
 
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    //buildGauge(data.WFREQ);
+  });
 }
 
-function buildCharts(sample) {
+function buildCharts(sample_response) {
+  //var bubble//chart_url = `/samples/${sample_response}`      
+  //var piechart_url = `/samples/${sample_response}`
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+ // @TODO: Build a Bubble Chart using the sample data
+  d3.json(`/samples/${sample_response}`).then(function (sample_response){
+    renderBubbleChart(sample_response);
+  });
+  
+  // @TODO: Build a Pie Chart
+  console.log("in pie chart")
+  d3.json(`/samples/${sample_response}`).then(function (sample_response){
+    renderPieChart(sample_response);
+  });
+}
 
-    // @TODO: Build a Bubble Chart using the sample data
+function renderPieChart(responseDict) {
+  var data = [{
+    values: responseDict.sample_values.slice(0,10),
+    labels: responseDict.otu_ids.slice(0,10),
+    hovertext: responseDict.otu_labels.slice(0,10),
+    type: 'pie'
+  }];
+  var layout = {
+      title: '% of Samples Observed',
+      margin: { t: 0, l: 0 }
+  };
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+  Plotly.newPlot('pie', data, layout);
+}
+
+function renderBubbleChart(responseDict) {
+  //console.log("This is the response Dict: ",responseDict)
+  let xBubble = responseDict.otu_ids
+  let yBubble = responseDict.sample_values
+  let bubbleLabels = responseDict.otu_labels
+  let bubbleMarker = responseDict.sample_values
+  let bubbleColors = responseDict.otu_ids
+  let data = {
+    x: xBubble,
+    y: yBubble,
+    mode:"markers",
+    marker:{ size:bubbleMarker, color:bubbleColors},
+    text: bubbleLabels
+  }
+  bubbleData = [data];
+
+  let layout = {
+    title: 'Belly Button sample', 
+    xaxis: {title: "Otu ID"},
+    yaxis: {title: "Sample Value"},
+    width: 1100,
+    height: 500,
+    hovermode: 'closest',
+  };
+
+  Plotly.newPlot('bubble', bubbleData, layout)
 }
 
 function init() {
@@ -65,7 +98,6 @@ function init() {
 }
 
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
 }
